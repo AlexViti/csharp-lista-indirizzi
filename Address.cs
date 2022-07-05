d16e6bed3dcbcc4db413c8d12bedafc668198dd5
AlexViti <alexviti12@gmail.com>
2022-07-05 14:09:48 +0200

    Add Address class

---
+++ Address.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaIndirizzi
+{
+    internal class Address
+    {
+        // Name,Surname,Street,City,Province,ZIP
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string Province { get; set; }
+        public int ZIP { get; set; }
+
+        public Address(string name, string surname, string street, string city, string province, int zip)
+        {
+            Name = name;
+            Surname = surname;
+            Street = street;
+            City = city;
+            Province = province;
+            ZIP = zip;
+        }
+
+        public string PaddedZIP()
+        {
+            return ZIP.ToString().PadLeft(5, '0');
+        }
+    }
+}
